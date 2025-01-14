0cdd476f427275baa48d54c929470e7bb06a0cc9
Liviu <liviu.bancila95@gmail.com>
2025-01-14 18:05:32 +0200

    .

--- app/wizard/page.tsx
+++ app/wizard/page.tsx
@@ -23,7 +23,7 @@ async function page() {
     <div className="container flex max-w-2xl flex-col items-center justify-between gap-4">
       <div>
         <h1 className="text-center text-3xl">
-          Welcome, Liviu <span className="ml-2 font-bold">{user.firstName}! 👋</span>
+          Welcome, <span className="ml-2 font-bold">{user.firstName}! 👋</span>
         </h1>
         <h2 className="mt-4 text-center text-base text-muted-foreground">
           Let &apos;s get started by setting up your currency
