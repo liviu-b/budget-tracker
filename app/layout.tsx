24fc161ebf1693705618d4b294941148637df7ef
Liviu <liviu.bancila95@gmail.com>
2025-01-15 14:09:21 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Budget Tracker",
+  title: "SmartBudget",
   description: "Your Financial Companion",
 };
 export default function RootLayout({
